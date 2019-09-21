4d4b4f688f99878cb4f78b0229323836db9632e4
Kelly Selden <kellyselden@gmail.com>
2019-09-21 20:30:12 +0100

    fix: remove `findBin`

--- helpers/chai.js
+++ helpers/chai.js
@@ -3,7 +3,6 @@
 const chai = require('chai');
 
 chai.use(require('@faltest/chai'));
-chai.use(require('chai-fs'));
 chai.use(require('chai-string'));
 chai.use(require('chai-as-promised'));
 chai.use(require('sinon-chai'));
