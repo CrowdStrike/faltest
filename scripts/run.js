c2ef5cdecb91cabaf809c543942e9abfe832ca36
Kelly Selden <kellyselden@gmail.com>
2019-09-21 10:11:29 +0100

    fix: run examples with xvfb

---
+++ scripts/run.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const execa = require('execa');
+const path = require('path');
+
+const [packagePrefix, scriptName] = process.argv.slice(2);
+
+(async () => {
+  let cp = await execa.command('yarn --silent workspaces info');
+
+  let json = JSON.parse(cp.stdout);
+
+  for (let { location } of Object.values(json)) {
+    if (!location.startsWith(packagePrefix)) {
+      continue;
+    }
+
+    let cwd = path.resolve(__dirname, '..', location);
+
+    await execa('yarn', [scriptName], {
+      cwd,
+      stdio: 'inherit',
+    });
+  }
+})();
