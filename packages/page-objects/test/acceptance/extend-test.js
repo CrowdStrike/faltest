af8f475314e8f8d95ba4349828c5bc509750fd11
Kelly Selden <kellyselden@gmail.com>
2019-09-24 15:19:06 +0100

    fix: allow empty string to ignore extend class

---
+++ packages/page-objects/test/acceptance/extend-test.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const { describe, it } = require('../../../../helpers/mocha');
+const { expect } = require('../../../../helpers/chai');
+const { setUpWebDriver } = require('@faltest/lifecycle');
+const { killOrphans } = require('@faltest/remote');
+const {
+  BasePageObject,
+  Element,
+  Elements,
+} = require('../../src');
+const Server = require('../../../../helpers/server');
+
+describe(function() {
+  setUpWebDriver.call(this, {
+    shareWebdriver: true,
+    keepBrowserOpen: true,
+    overrides: {
+      browser: 'chrome',
+      waitforTimeout: 0,
+    },
+  });
+
+  let server;
+
+  before(async function() {
+    server = new Server();
+
+    let port = await server.start();
+
+    await this.browser.url(`http://localhost:${port}/page-objects.html`);
+
+    this.createPage = function(Page) {
+      return new Page(this.browser);
+    };
+  });
+
+  after(async function() {
+    if (server) {
+      await server.stop();
+    }
+
+    await killOrphans();
+  });
+
+  it('can override a default class via empty string', async function() {
+    class Single1 extends Element {}
+    class Multiple1 extends Elements {}
+
+    Single1.selector = '.missing';
+    Multiple1.selector = '.missing';
+
+    let page = this.createPage(class extends BasePageObject {
+      get multiple1() {
+        return this._create('.multiple1', ({ extend, extendMany }) => ({
+          first: extend(Single1, ''),
+          all: extendMany(Multiple1, ''),
+        }));
+      }
+    });
+
+    await expect(page.multiple1.first).text.to.eventually.equal('08IcL2gsWh');
+    await expect(page.multiple1.all).elements.to.eventually.have.a.lengthOf(2);
+  });
+});
