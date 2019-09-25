2876a0c3af68e8b7dc300b28def81742df19062e
Kelly Selden <kellyselden@gmail.com>
2019-09-25 18:51:06 +0100

    feat: add page object `setPassword`

--- packages/page-objects/src/element.js
+++ packages/page-objects/src/element.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const BaseElement = require('./base-element');
+const { setPassword } = require('@faltest/remote');
 
 class Element extends BaseElement {
   async getElement() {
@@ -41,6 +42,10 @@ class Element extends BaseElement {
 
   async setValue() {
     await this._browser.setValue(this._selector, ...arguments);
+  }
+
+  async setPassword() {
+    await setPassword(await this.getElement(), ...arguments);
   }
 
   async sendKeys() {
