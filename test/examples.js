945500cf6c2a3c76faf9d6159c717daac8ae693a
Kelly Selden <kellyselden@gmail.com>
2019-10-07 19:22:57 +0100

    chore: switch to mocha for examples tests
    and expand test cases

---
+++ test/examples.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const { describe, it } = require('../helpers/mocha');
+const { expect } = require('../helpers/chai');
+const execa = require('execa');
+const path = require('path');
+
+function getCwd(example) {
+  return path.resolve(__dirname, '../examples', example);
+}
+
+describe(function() {
+  before(function() {
+    this.run = async function run() {
+      try {
+        let { stdout } = await execa.command(this.test.title, {
+          cwd: this.test.parent.title,
+        });
+
+        return stdout;
+      } catch (err) {
+        return err.stdout;
+      }
+    };
+  });
+
+  describe(getCwd('custom-cli'), function() {
+    it('yarn start', async function() {
+      this.timeout(30 * 1000);
+
+      let output = await this.run();
+
+      expect(output).to.include(' 1 passing');
+    });
+  });
+
+  describe(getCwd('full-suite'), function() {
+    it('yarn start --target fixtures --env dev --tag user --tag smoke', async function() {
+      this.timeout(30 * 1000);
+
+      let output = await this.run();
+
+      expect(output).to.include(' 1 passing');
+      expect(output).to.not.include(' pending');
+    });
+
+    it('yarn start --tag admin --tag !smoke', async function() {
+      this.timeout(60 * 1000);
+
+      let output = await this.run();
+
+      expect(output).to.include(' 2 passing');
+      expect(output).to.include(' 1 pending');
+    });
+
+    it('yarn start --tag admin --filter unfinished', async function() {
+      this.timeout(30 * 1000);
+
+      let output = await this.run();
+
+      expect(output).to.include(' 1 passing');
+      expect(output).to.not.include(' pending');
+    });
+  });
+
+  describe(getCwd('lifecycle-only'), function() {
+    it('yarn start', async function() {
+      this.timeout(30 * 1000);
+
+      let output = await this.run();
+
+      expect(output).to.include(' 1 passing');
+    });
+  });
+
+  describe(getCwd('multiple-browsers'), function() {
+    it('yarn start', async function() {
+      this.timeout(30 * 1000);
+
+      let output = await this.run();
+
+      expect(output).to.include(' 1 passing');
+    });
+  });
+
+  describe(getCwd('runner-only'), function() {
+    it('yarn start', async function() {
+      this.timeout(30 * 1000);
+
+      let output = await this.run();
+
+      expect(output).to.include(' 1 passing');
+    });
+  });
+});
