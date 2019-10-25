0d696137a8b441c64e094bb7003ad66b052c4d54
Kelly Selden <kellyselden@gmail.com>
2019-10-25 16:03:39 +0100

    fix: pipe test output from execa

--- helpers/run.js
+++ helpers/run.js
@@ -4,7 +4,12 @@ const execa = require('execa');
 
 async function run(command, options) {
   try {
-    let { stdout } = await execa.command(command, options);
+    let cp = execa.command(command, options);
+
+    cp.stdout.pipe(process.stdout);
+    cp.stderr.pipe(process.stderr);
+
+    let { stdout } = await cp;
 
     return stdout;
   } catch (err) {
