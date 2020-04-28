1af8786ea3cca8f23bb13e3f0934f14ad3f82da1
Kelly Selden <kellyselden@gmail.com>
2020-04-28 14:04:29 +0100

    fix: consume the new chai properties object

--- packages/lint/test/rules/chai-webdriver-eventually-test.js
+++ packages/lint/test/rules/chai-webdriver-eventually-test.js
@@ -4,6 +4,8 @@ const { RuleTester } = require('eslint');
 const rule = require('../../src/rules/chai-webdriver-eventually');
 const { properties } = require('@faltest/chai');
 
+const propertyKeys = Object.keys(properties);
+
 describe('', function() {
   new RuleTester().run('chai-webdriver-eventually', rule, {
     valid: [
@@ -11,14 +13,14 @@ describe('', function() {
       `expect()().be`, // non-MemberExpression
       `foo().to.be`,
       `foo().to.eventually.be`,
-      `foo().${properties[0]}.to.be`,
-      `expect().${properties[0]}.to.eventually.be`,
+      `foo().${propertyKeys[0]}.to.be`,
+      `expect().${propertyKeys[0]}.to.eventually.be`,
     ],
     invalid: [
       {
-        code: `expect().${properties[0]}.to.be`,
+        code: `expect().${propertyKeys[0]}.to.be`,
         errors: [{
-          message: `You must add \`eventually\` when using Chai WebDriver property \`${properties[0]}\`.`,
+          message: `You must add \`eventually\` when using Chai WebDriver property \`${propertyKeys[0]}\`.`,
           type: 'ExpressionStatement',
         }],
       },
