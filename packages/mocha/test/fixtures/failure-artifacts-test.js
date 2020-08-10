3075f5cf471127d6c87355a09a514173738d126c
Kelly Selden <kellyselden@gmail.com>
2020-08-10 12:08:22 +0100

    feat: optimize failure artifacts
    We can remove the try/catch override for every hook by using `this.currentTest.state` in `afterEach` instead.
    
    BREAKING CHANGE: `createFailureArtifacts` was removed because it is no longer necessary.

--- packages/mocha/test/fixtures/failure-artifacts-test.js
+++ packages/mocha/test/fixtures/failure-artifacts-test.js
@@ -1,10 +1,7 @@
 'use strict';
 
 const assert = require('assert');
-const { createFailureArtifactsHelpers } = require('../../src');
 const { setUpWebDriver } = require('../../../lifecycle');
-
-Object.assign(global, createFailureArtifactsHelpers(global));
 
 describe('failure artifacts', function() {
   setUpWebDriver.call(this);
