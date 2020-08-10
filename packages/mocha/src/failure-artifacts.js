f5dcb2a370517c18d25c83417f8aca6601e44f5a
Kelly Selden <kellyselden@gmail.com>
2020-08-10 12:08:22 +0100

    feat: optimize failure artifacts
    We can remove the try/catch override for every hook by using `this.currentTest.state` in `afterEach` instead.
    
    BREAKING CHANGE: `createFailureArtifacts` was removed because it is no longer necessary.

--- packages/mocha/src/failure-artifacts.js
+++ packages/mocha/src/failure-artifacts.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const { wrap } = require('./mocha');
 const path = require('path');
 const fs = require('fs');
 const { promisify } = require('util');
@@ -8,94 +7,48 @@ const writeFile = promisify(fs.writeFile);
 const mkdir = promisify(fs.mkdir);
 const filenamify = require('filenamify');
 
-function failureArtifacts(outputDir) {
-  return test => {
-    return function failureArtifacts(...args) {
-      function buildTitle(test) {
-        let parts = [];
+function buildTitle(test) {
+  let parts = [];
 
-        while (test) {
-          if (test.title) {
-            parts.unshift(test.title);
-          }
+  while (test) {
+    if (test.title) {
+      parts.unshift(test.title);
+    }
 
-          test = test.parent;
-        }
-
-        let title = parts.join(' ');
-
-        // Tests with / in the name are bad.
-        title = filenamify(title);
-
-        return title;
-      }
-
-      let callback = args.pop();
-
-      return test(...args, async function() {
-        try {
-          return await callback.apply(this, arguments);
-        } catch (err) {
-          if (this.browser) {
-            let title = buildTitle(this.test);
-            try {
-              await mkdir(outputDir);
-            } catch (err) {}
-
-            let element = await this.browser.$('body');
-
-            let screenshot = await this.browser._browser.takeElementScreenshot(element.elementId);
-            let screenshotPath = path.join(outputDir, `${title}.png`);
-            await writeFile(screenshotPath, screenshot, 'base64');
-
-            let html = await element.getHTML();
-            let htmlPath = path.join(outputDir, `${title}.html`);
-            await writeFile(htmlPath, html);
-
-            let logTypes = await this.browser._browser.getLogTypes();
-            for (let logType of logTypes) {
-              let logs = await this.browser._browser.getLogs(logType);
-              let logsText = JSON.stringify(logs, null, 2);
-              let logPath = path.join(outputDir, `${title}.${logType}.txt`);
-              await writeFile(logPath, logsText);
-            }
-          }
-          throw err;
-        }
-      });
-    };
-  };
-}
-
-
-function create(mocha, {
-  enabled = process.env.WEBDRIVER_FAILURE_ARTIFACTS === 'true',
-  outputDir = process.env.WEBDRIVER_FAILURE_ARTIFACTS_OUTPUT_DIR,
-} = {}) {
-  if (enabled && !outputDir) {
-    throw new Error('You must supply an output dir.');
+    test = test.parent;
   }
 
-  let __failureArtifacts = failureArtifacts(outputDir);
+  let title = parts.join(' ');
 
-  return [
-    'before',
-    'beforeEach',
-    'it',
-    'afterEach',
-    'after',
-  ].reduce((hooks, lifecycle) => {
-    if (mocha[lifecycle]) {
-      if (enabled) {
-        hooks[lifecycle] = wrap(mocha[lifecycle], __failureArtifacts);
-      } else {
-        hooks[lifecycle] = mocha[lifecycle];
-      }
-    }
-    return hooks;
-  }, {});
+  // Tests with / in the name are bad.
+  title = filenamify(title);
+
+  return title;
 }
 
-module.exports = {
-  create,
-};
+async function failureArtifacts(outputDir) {
+  let title = buildTitle(this.currentTest);
+  try {
+    await mkdir(outputDir);
+  } catch (err) {}
+
+  let element = await this.browser.$('body');
+
+  let screenshot = await this.browser._browser.takeElementScreenshot(element.elementId);
+  let screenshotPath = path.join(outputDir, `${title}.png`);
+  await writeFile(screenshotPath, screenshot, 'base64');
+
+  let html = await element.getHTML();
+  let htmlPath = path.join(outputDir, `${title}.html`);
+  await writeFile(htmlPath, html);
+
+  let logTypes = await this.browser._browser.getLogTypes();
+  for (let logType of logTypes) {
+    let logs = await this.browser._browser.getLogs(logType);
+    let logsText = JSON.stringify(logs, null, 2);
+    let logPath = path.join(outputDir, `${title}.${logType}.txt`);
+    await writeFile(logPath, logsText);
+  }
+}
+
+module.exports = failureArtifacts;
