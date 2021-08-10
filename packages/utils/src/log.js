75aab7766049bf1a029221cbee36be84c127b508
Kelly Selden <kellyselden@gmail.com>
2021-08-10 10:05:04 -0700

    fix: fix callsites import

--- packages/utils/src/log.js
+++ packages/utils/src/log.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const callsites = require('callsites');
 
 function log(name) {
   const debug = require('./debug')(name);
 
   return async function log(blockName, callback) {
     if (!callback) {
+      const { default: callsites } = await import('callsites');
+
       let callerName = callsites()[1].getFunctionName();
 
       callback = blockName;
