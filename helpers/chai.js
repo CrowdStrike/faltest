0a111f72d9a6d43dc2beb6cd219a1eaee6fecfb2
Kelly Selden <kellyselden@gmail.com>
2023-01-08 15:58:53 -0800

    chore: improve tests

--- helpers/chai.js
+++ helpers/chai.js
@@ -1,11 +1,32 @@
 'use strict';
 
 const chai = require('chai');
+const sinon = require('sinon');
 
 chai.use(require('../packages/chai'));
 chai.use(require('chai-string'));
 chai.use(require('chai-fs'));
 chai.use(require('chai-as-promised'));
 chai.use(require('sinon-chai'));
+
+// This should be replaced by Chai v5
+// https://github.com/chaijs/chai/issues/644
+chai.use((_chai, utils) => {
+  chai.Assertion.addMethod('matches', function match(expectedMatch) {
+    let subject = utils.flag(this, 'object');
+    let spy = sinon.spy();
+    spy(subject);
+    try {
+      sinon.assert.calledWithMatch(spy, expectedMatch);
+    } catch (error) {
+      error.name = 'MatchAssertionError';
+      error.message = error.message.replace(
+        /^expected spy to be called with match/,
+        `expected ${utils.objDisplay(subject)} to match`,
+      );
+      throw error;
+    }
+  });
+});
 
 module.exports = chai;
