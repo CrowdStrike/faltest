e305675430884ebf667a90fe05bd2ba0cbde2d13
Kelly Selden <kellyselden@gmail.com>
2023-07-07 17:39:12 -0700

    chore: use npm instead of yarn in global install test
    yarn@1 is resolving incorrectly
    Error [ERR_REQUIRE_ESM]: require() of ES Module /node_modules/string-width/index.js from /node_modules/cliui/build/index.cjs not supported.
    similar to https://github.com/nrwl/nx/issues/17229

--- scripts/postbump.js
+++ scripts/postbump.js
@@ -6,7 +6,10 @@ const path = require('path');
 (async () => {
   const { execa } = await import('execa');
 
-  await execa('yarn', ['install', '--force'], {
+  // yarn@1 is resolving incorrectly
+  // Error [ERR_REQUIRE_ESM]: require() of ES Module /node_modules/string-width/index.js from /node_modules/cliui/build/index.cjs not supported.
+  // await execa('yarn', ['install', '--force'], {
+  await execa('npm', ['install'], {
     cwd: path.resolve(__dirname, '../fixtures/global-install'),
     stdio: 'inherit',
   });
