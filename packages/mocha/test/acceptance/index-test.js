b2640986ced3056a317d7f4508fb0b623bbeff4d
Kelly Selden <kellyselden@gmail.com>
2023-07-10 20:21:01 -0700

    chore: add test for failure artifact filename length

--- packages/mocha/test/acceptance/index-test.js
+++ packages/mocha/test/acceptance/index-test.js
@@ -413,6 +413,19 @@ describe(function() {
           });
         });
       });
+
+      it('handles long test names', async function() {
+        let stats = await this.runTests({
+          filter: 'it chrome This is a really long test name This is a really long test name This is a really long test name$',
+        });
+
+        expect(stats).matches(sinon.match({
+          tests: 1,
+          failures: 1,
+        }));
+
+        this.assertFilesExist('it chrome This is a really long test name This is a really long test name This is ');
+      });
     });
 
     describe('dry run', function() {
